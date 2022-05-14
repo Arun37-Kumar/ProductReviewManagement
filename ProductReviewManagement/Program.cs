ddcb92270d457600f676bf0bfda867b605d13010
Arun37-Kumar <arun007624@gmail.com>
2022-05-14 10:07:10 +0530

    UC1 updated

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -10,7 +10,23 @@ namespace ProductReviewManagement
     {
         static void Main(string[] args)
         {
+            ReviewManager manage = new ReviewManager();
+            List<ProductReview> products = new List<ProductReview>();
 
+            Console.WriteLine("1 : Add Product Review ");
+            Console.Write("Enter the option  : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: // Adding the product review and showing the list
+                    manage.AddProductReview(products);
+                    manage.ShowList(products);
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
